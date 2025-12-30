63e831e438b621ab2f0a2fc87c2430bb73529964
nealwicke <neal@jobtoolz.com>
2025-12-30 21:46:52 +0100

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/wickejeroen.be/build/' : './',
+    base: process.env.NODE_ENV === 'production' ? '/build/' : './',
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
